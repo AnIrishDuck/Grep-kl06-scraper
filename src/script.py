3c26bddddbd0589a8e81db30decb445d9c739d9b
Jose Pintado <jospint@gmail.com>
2018-05-17 22:02:57 +0200

    Removed many whitespaces and unnecessary comments

--- src/script.py
+++ src/script.py
@@ -5,42 +5,27 @@ from pathlib import Path
 import io
 
 BASE_URL = 'http://data.udir.no/kl06/'
+UTF8 = 'utf-8'
 
 def main():
 	#Parsing blob with all study plans
 	norwayplans = json.load(urlopen(BASE_URL + 'laereplaner.json'))
-
 	#getting information for each study plan
 	scrappedplans = []
 	for studyplan in norwayplans:
 		studyplandetail = get_study_plan_detail(studyplan)
 		scrappedstudyplan = {}
-
-		#study plan code
 		scrappedstudyplan['code'] = studyplandetail['kode']
-
-		#study plan name
-		scrappedstudyplan['name'] = parse_title(studyplandetail)
-		
-		#study plan main areas
+		scrappedstudyplan['name'] = parse_title(studyplandetail)		
 		scrappedstudyplan['main_areas'] = [] if studyplandetail['hovedomraade-kapittel'] is None else [parse_title(area) for area in studyplandetail['hovedomraade-kapittel']['hovedomraader']]
-
 		#getting information for each study plan subject
 		scrappedstudyplan['subjects'] = []
 		for subject in studyplandetail['kompetansemaal-kapittel']['kompetansemaalsett']:
 			scrappedsubject = {}
-
-			#subject name
 			scrappedsubject['name'] = parse_title(subject)
-
-			#subject main areas
 			scrappedsubject['main_areas'] = [parse_title(area['hovedomraadeverdier-under-kompetansemaalsett']) for area in subject['hovedomraader-i-kontekst-av-kompetansemaalsett']]
-
-			#subject goals
 			scrappedsubject['goals'] = [goal['tittel'] for goal in subject['kompetansemaal']]
-
 			scrappedstudyplan['subjects'].append(scrappedsubject)
-
 		scrappedplans.append(scrappedstudyplan);
 	print(json.dumps(scrappedplans))
 
@@ -50,11 +35,11 @@ def get_study_plan_detail(studyplan):
 	#if we did not save locally the json file, we do it now
 	if not Path(studyplanfilename).is_file():	
 		studyplandetail = json.load(urlopen(BASE_URL + studyplanfilename))
-		with open(studyplanfilename, 'w', encoding='utf-8') as fi:
+		with open(studyplanfilename, 'w', encoding=UTF8) as fi:
 			json.dump(studyplandetail, fi)
 	#else we load it from local folder		
 	else:
-		with open(studyplanfilename, encoding='utf-8') as fi:
+		with open(studyplanfilename, encoding=UTF8) as fi:
 			studyplandetail = json.load(fi)
 	return studyplandetail
 
